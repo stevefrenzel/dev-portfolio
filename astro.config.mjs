5916813930761cd220200aaed5566d757108df5c
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2023-06-10 14:56:16 +0200

    Squashed commit of the following:
    commit 19777f78217763efa556a3e529c63cd77867b614
    Author: Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
    Date:   Sat Jun 10 14:53:38 2023 +0200
    
        Update design
    
    commit db703e0469ba05bee8e802a84e6c8c128d44d65d
    Author: Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
    Date:   Sat Jun 10 13:27:57 2023 +0200
    
        Clean-up
    
    commit ea7c8facefdd65cb4232d86eccab74b1d4076fe8
    Author: Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
    Date:   Sat Jun 10 12:22:44 2023 +0200
    
        Update package-lock.json
    
    commit 1d53c7e12fd0f07aedc30c0985e5fd1a9a3b050f
    Author: Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
    Date:   Sat Jun 10 12:21:42 2023 +0200
    
        Add navigation, footer & SEO
    
    commit dd460fc573c07f3d9a8290652c13e207bd2732c4
    Author: Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
    Date:   Fri Jun 9 12:50:19 2023 +0200
    
        Add anchor links, form validation
    
    commit 0d678eeb3d6da697630a8732a93f0c1207339aa2
    Author: Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
    Date:   Thu Jun 8 14:37:05 2023 +0200
    
        Update font, type and look

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 
 export default defineConfig({
-  site: 'https://stevefrenzel.github.io',
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
