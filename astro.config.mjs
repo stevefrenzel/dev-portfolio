939d48e50b83bf4e54e91401438f6bc75e73a926
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2023-06-10 13:39:35 +0200

    Add GitHub actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 
 export default defineConfig({
+  site: 'https://stevefrenzel.github.io',
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
