686d0c9a66ba438df1a2a385ef19b6eb78c2f63e
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2023-06-23 11:25:00 +0200

    Squashed commit of the following:
    commit 257919b4092fa6fee1fd93e7de2da8e42d2d8ebe
    Author: Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
    Date:   Fri Jun 23 11:22:49 2023 +0200
    
        Add blog functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
   integrations: [
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    ,
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page',
+      },
     }),
   ],
 });
