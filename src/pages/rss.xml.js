ee462e4746e39ea5a66441a15c59d493473d9596
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2023-11-20 14:12:02 +0100

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const posts = await getCollection("posts");
+  const allPosts = await getCollection("blog");
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: context.site,
-    items: posts.map((post) => ({
+    items: allPosts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
