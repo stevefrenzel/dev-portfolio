1e9e8df2a2566fd3c4459d0bacbd3882b916fe36
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2024-01-13 14:05:55 +0100

    Update blog page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  compressHTML: true,
   site: "https://www.stevefrenzel.dev",
   integrations: [mdx()],
 });
