f8f488e5c0ddf851e2e683e3ad3473626b765ba6
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2024-02-13 10:22:20 +0100

    Add reading time to blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://www.stevefrenzel.dev",
-  integrations: [mdx()],
 });
