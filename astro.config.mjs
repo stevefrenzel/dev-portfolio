584e856d3aa17997cf2be93eb5d57f5233872e3e
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2024-10-20 08:27:14 +0200

    Added MDX (again)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
   site: "https://www.stevefrenzel.dev",
+  integrations: [mdx()],
 });
