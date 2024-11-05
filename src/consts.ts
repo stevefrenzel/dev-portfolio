ff4fff31aec59da7d87e6a845db6be6e999e9863
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2024-11-05 07:11:57 +0100

    Update wording

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = "Blog by Steve Frenzel";
 export const SITE_DESCRIPTION =
-  "A blog about web accessibility, nerdy music stuff, everything beyond and in-between by Steve Frenzel. Fuck off if these things offend you: 🏳️‍⚧️ 🏳️‍🌈 ✊🏿 🇺🇦 🇵🇸";
+  "A blog about web accessibility, nerdy music stuff, everything beyond and in-between by Steve Frenzel. Go away if these things offend you: 🏳️‍⚧️ 🏳️‍🌈 ✊🏿 🇺🇦 🇵🇸";
